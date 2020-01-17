86d718b899f5c9ab50f618ff7cc7d4f066987bff
Ilya Gushchin <frylock3434@gmail.com>
2020-01-17 11:57:11 +0400

    add translation extraction script

---
+++ docker/extract-translations.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Abort at the first error.
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PROJECT_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
+
+# Make sure that environment variables are set properly
+source /opt/rh/devtoolset-7/enable
+export PATH="${CURA_BUILD_ENV_PATH}/bin:${PATH}"
+export PKG_CONFIG_PATH="${CURA_BUILD_ENV_PATH}/lib/pkgconfig:${PKG_CONFIG_PATH}"
+
+cd "${PROJECT_DIR}"
+
+#
+# Clone Uranium and set PYTHONPATH first
+#
+URANIUM_BRANCH="steslicer"
+output="$(git ls-remote --heads https://gitlab.com/stereotech/steslicer/Uranium.git "${URANIUM_BRANCH}")"
+if [ -z "${output}" ]; then
+    echo "Could not find Uranium banch ${URANIUM_BRANCH}, fallback to use master."
+    URANIUM_BRANCH="master"
+fi
+
+echo "Using Uranium branch ${URANIUM_BRANCH} ..."
+git clone --depth=1 -b "${URANIUM_BRANCH}" https://gitlab.com/stereotech/steslicer/Uranium.git "${PROJECT_DIR}"/Uranium
+export PYTHONPATH="${PROJECT_DIR}/Uranium:.:${PYTHONPATH}"
+
+mkdir build
+cd build
+cmake3 \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_PREFIX_PATH="${CURA_BUILD_ENV_PATH}" \
+    -DURANIUM_DIR="${PROJECT_DIR}/Uranium" \
+    -DBUILD_TESTS=ON \
+    ..
+make extract-messages
