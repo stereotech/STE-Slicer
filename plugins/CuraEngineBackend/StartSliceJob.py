94e183001ee6b2d1a20cfae35b57613f92bef7ed
Ilya Gushchin <frylock3434@gmail.com>
2021-12-22 10:07:31 +0300

    closes #63

--- plugins/CuraEngineBackend/StartSliceJob.py
+++ plugins/CuraEngineBackend/StartSliceJob.py
@@ -584,6 +584,8 @@ class StartSliceJob(Job):
 
             settings["layer_height"] = settings["classic_layer_height"]
 
+            settings["support_enable"] = settings["support_enable_classic"]
+
         # Add all sub-messages for each individual setting.
         for key, value in settings.items():
             setting_message = self._slice_message.getMessage(
