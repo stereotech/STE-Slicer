14406744cd85ecf496c10af8035e1570f37c9ade
Oleg-SH88 <102018920+Oleg-SH88@users.noreply.github.com>
2023-03-07 12:30:34 +0300

    SLICER-162 fixed a problem with building supports

--- plugins/CLIParserBackend/StartSliceJob.py
+++ plugins/CLIParserBackend/StartSliceJob.py
@@ -865,8 +865,6 @@ class StartSliceJob(Job):
                             setting_value = "0"
                     if name in ["rsize", "first_offset", "last_offset", "support_base_r"]:
                         setting_value /= 2
-                        #if name == "support_base_r":
-                        #    setting_value += settings.get("cylindrical_layer_height", 0.2)
                         if name == "first_offset" and region =="GCodeSupport":
                             setting_value = settings.get("support_first_offset")
                     if name in ["upskin_width", "downskin_width"]:
